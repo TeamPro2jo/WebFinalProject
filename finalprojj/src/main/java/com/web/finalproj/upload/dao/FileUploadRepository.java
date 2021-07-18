471419a5da287a6cb93f80fb367aa5316a676d50
hakyung0426 <60084035+hakyung0426@users.noreply.github.com>
2021-07-18 23:43:55 +0900

    Delete finalprojj directory

--- finalprojj/src/main/java/com/web/finalproj/upload/dao/FileUploadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.web.finalproj.upload.dao;
-
-import java.util.HashMap;
-
-public interface FileUploadRepository {
-	public int insert(int id, HashMap<String, String> data) throws Exception;
-}
--- finalprojj/src/main/java/com/web/finalproj/upload/service/FileUploadService.java
+++
@@ -1,9 +0,0 @@
-package com.web.finalproj.upload.service;
-
-import java.util.HashMap;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileUploadService {
-	public int save(int id, HashMap<String, String> data) throws Exception;
-}
